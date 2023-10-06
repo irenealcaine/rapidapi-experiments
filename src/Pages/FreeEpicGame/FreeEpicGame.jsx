ef1f395ed6a913b77cefb0367e29160ef8c975a2
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-10-06 16:54:15 +0200

    update FreeEpicGame.jsx, FreeGames.jsx and FreeGames.scss

--- src/Pages/FreeEpicGame/FreeEpicGame.jsx
+++ src/Pages/FreeEpicGame/FreeEpicGame.jsx
@@ -4,64 +4,64 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const FreeEpicGame = () => {
-  // const [data, setData] = useState([]);
+  const [gameData, setGameData] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get(process.env.REACT_APP_URL_DE_LA_API, {
-  //         headers: {
-  //           'X-RapidAPI-Host': process.env.REACT_APP_TU_RAPIDAPI_HOST,
-  //           'X-RapidAPI-Key': process.env.REACT_APP_TU_RAPIDAPI_KEY,
-  //         },
-  //       });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(process.env.REACT_APP_URL_DE_LA_API, {
+          headers: {
+            'X-RapidAPI-Host': process.env.REACT_APP_TU_RAPIDAPI_HOST,
+            'X-RapidAPI-Key': process.env.REACT_APP_TU_RAPIDAPI_KEY,
+          },
+        });
 
-  //       setData(response.data);
-  //       console.log(data)
-  //     } catch (error) {
-  //       console.error('Error al obtener datos:', error);
-  //     }
-  //   };
+        setGameData(response.data);
+        // console.log(gameData)
+      } catch (error) {
+        console.error('Error al obtener datos:', error);
+      }
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
-  const exampleData = [
-    {
-      name: "Epistory - Typing Chronicles",
-      description:
-        "Epistory immerses you in an atmospheric game where you play a girl riding a giant fox who fights an insectile corruption from an origami world. As you progress and explore this world, the story literally unfolds and the mysteries of the magic power of the words are revealed.",
-      offerImageWide:
-        "https://cdn1.epicgames.com/spt-assets/2fe270709f944fc398851454cf476f95/epistory-typing-adventure-1t9zx.jpg",
-      offerImageTall:
-        "https://cdn1.epicgames.com/spt-assets/2fe270709f944fc398851454cf476f95/epistory--typing-chronicles-19b5z.png",
-      publisher: "Plug In Digital SAS",
-      discountPrice: 0,
-      originalPrice: 1499,
-      currencyCode: "USD",
-      appUrl:
-        "https://store.epicgames.com/en-US/p/epistory-typing-chronicles-445794",
-    },
-    {
-      name: "Godlike Burger",
-      description:
-        "In Godlike Burger you run the craziest restaurant of the galaxy! Stun, poison and kill customers in many devious ways… and turn them into burger meat! And don’t worry: Alien customers will keep coming if you are smart about it, because cannibalism is just too damn tasty.",
-      offerImageWide:
-        "https://cdn1.epicgames.com/spt-assets/f42598038b9343e58d27e0a8c0b831b6/godlike-burger-offer-1trpc.jpg",
-      offerImageTall:
-        "https://cdn1.epicgames.com/spt-assets/f42598038b9343e58d27e0a8c0b831b6/download-godlike-burger-offer-8u2uh.jpg",
-      publisher: "Daedalic Entertainment",
-      discountPrice: 0,
-      originalPrice: 1999,
-      currencyCode: "USD",
-      appUrl: "https://store.epicgames.com/en-US/p/godlike-burger-4150a0",
-    },
-  ];
+  // const exampleData = [
+  //   {
+  //     name: "Epistory - Typing Chronicles",
+  //     description:
+  //       "Epistory immerses you in an atmospheric game where you play a girl riding a giant fox who fights an insectile corruption from an origami world. As you progress and explore this world, the story literally unfolds and the mysteries of the magic power of the words are revealed.",
+  //     offerImageWide:
+  //       "https://cdn1.epicgames.com/spt-assets/2fe270709f944fc398851454cf476f95/epistory-typing-adventure-1t9zx.jpg",
+  //     offerImageTall:
+  //       "https://cdn1.epicgames.com/spt-assets/2fe270709f944fc398851454cf476f95/epistory--typing-chronicles-19b5z.png",
+  //     publisher: "Plug In Digital SAS",
+  //     discountPrice: 0,
+  //     originalPrice: 1499,
+  //     currencyCode: "USD",
+  //     appUrl:
+  //       "https://store.epicgames.com/en-US/p/epistory-typing-chronicles-445794",
+  //   },
+  //   {
+  //     name: "Godlike Burger",
+  //     description:
+  //       "In Godlike Burger you run the craziest restaurant of the galaxy! Stun, poison and kill customers in many devious ways… and turn them into burger meat! And don’t worry: Alien customers will keep coming if you are smart about it, because cannibalism is just too damn tasty.",
+  //     offerImageWide:
+  //       "https://cdn1.epicgames.com/spt-assets/f42598038b9343e58d27e0a8c0b831b6/godlike-burger-offer-1trpc.jpg",
+  //     offerImageTall:
+  //       "https://cdn1.epicgames.com/spt-assets/f42598038b9343e58d27e0a8c0b831b6/download-godlike-burger-offer-8u2uh.jpg",
+  //     publisher: "Daedalic Entertainment",
+  //     discountPrice: 0,
+  //     originalPrice: 1999,
+  //     currencyCode: "USD",
+  //     appUrl: "https://store.epicgames.com/en-US/p/godlike-burger-4150a0",
+  //   },
+  // ];
 
   return (
     <div className="freeEpicGame">
       <h1>Free Epic Games</h1>
-      {exampleData.map((game) => (
+      {gameData.map((game) => (
         <Link key={game.name} className="container" to={game.appUrl}>
           <img
             className="tall-image"
--- src/Pages/FreeGames/FreeGames.jsx
+++ src/Pages/FreeGames/FreeGames.jsx
@@ -1,75 +1,101 @@
 import { Link } from "react-router-dom";
 import "./FreeGames.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 // https://rapidapi.com/digiwalls/api/free-to-play-games-database/
 // https://www.freetogame.com/api-doc
 
 const FreeGames = () => {
-  const exampleData = [
-    {
-      id: 540,
-      title: "Overwatch 2",
-      thumbnail: "https://www.freetogame.com/g/540/thumbnail.jpg",
-      short_description:
-        "A hero-focused first-person team shooter from Blizzard Entertainment.",
-      game_url: "https://www.freetogame.com/open/overwatch-2",
-      genre: "Shooter",
-      platform: "PC (Windows)",
-      publisher: "Activision Blizzard",
-      developer: "Blizzard Entertainment",
-      release_date: "2022-10-04",
-      freetogame_profile_url: "https://www.freetogame.com/overwatch-2",
-    },
-    {
-      id: 521,
-      title: "Diablo Immortal",
-      thumbnail: "https://www.freetogame.com/g/521/thumbnail.jpg",
-      short_description:
-        "A free-to-play, browser-based fantasy RPG developed \r\nby Game Hollywood and published by \r\nProficient City.",
-      game_url: "https://www.freetogame.com/open/diablo-immortal",
-      genre: "MMOARPG",
-      platform: "PC (Windows), Web Browser",
-      publisher: "Blizzard Entertainment",
-      developer: "Blizzard Entertainment",
-      release_date: "2022-06-02",
-      freetogame_profile_url: "https://www.freetogame.com/diablo-immortal",
-    },
-    {
-      id: 388,
-      title: "Lord’s Road",
-      thumbnail: "https://www.freetogame.com/g/388/thumbnail.jpg",
-      short_description:
-        "A free-to-play 2D browser-based fantasy MMORPG that features two playable classes.",
-      game_url: "https://www.freetogame.com/open/lords-road",
-      genre: "MMORPG",
-      platform: "Web Browser",
-      publisher: "AMZGame",
-      developer: "AMZGame",
-      release_date: "2015-06-03",
-      freetogame_profile_url: "https://www.freetogame.com/lords-road",
-    },
-    {
-      id: 181,
-      title: "8BitMMO",
-      thumbnail: "https://www.freetogame.com/g/181/thumbnail.jpg",
-      short_description:
-        "A free to play retro­-style 2D MMO and a giant construction sandbox! ",
-      game_url: "https://www.freetogame.com/open/8bitmmo",
-      genre: "MMORPG",
-      platform: "PC (Windows), Web Browser",
-      publisher: "Archive Entertainment ",
-      developer: "Archive Entertainment ",
-      release_date: "2015-01-26",
-      freetogame_profile_url: "https://www.freetogame.com/8bitmmo",
-    },
-  ];
+
+  const [freeGameData, setFreeGameData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(process.env.REACT_APP_API_URL, {
+          headers: {
+            'X-RapidAPI-Host': process.env.REACT_APP_HOST,
+            'X-RapidAPI-Key': process.env.REACT_APP_KEY,
+          },
+        });
+
+        setFreeGameData(response.data);
+      } catch (error) {
+        console.error('Error al obtener datos:', error);
+      }
+    };
+    fetchData();
+    console.log(freeGameData)
+
+  }, []);
+
+  // const exampleData = [
+  //   {
+  //     id: 540,
+  //     title: "Overwatch 2",
+  //     thumbnail: "https://www.freetogame.com/g/540/thumbnail.jpg",
+  //     short_description:
+  //       "A hero-focused first-person team shooter from Blizzard Entertainment.",
+  //     game_url: "https://www.freetogame.com/open/overwatch-2",
+  //     genre: "Shooter",
+  //     platform: "PC (Windows)",
+  //     publisher: "Activision Blizzard",
+  //     developer: "Blizzard Entertainment",
+  //     release_date: "2022-10-04",
+  //     freetogame_profile_url: "https://www.freetogame.com/overwatch-2",
+  //   },
+  //   {
+  //     id: 521,
+  //     title: "Diablo Immortal",
+  //     thumbnail: "https://www.freetogame.com/g/521/thumbnail.jpg",
+  //     short_description:
+  //       "A free-to-play, browser-based fantasy RPG developed \r\nby Game Hollywood and published by \r\nProficient City.",
+  //     game_url: "https://www.freetogame.com/open/diablo-immortal",
+  //     genre: "MMOARPG",
+  //     platform: "PC (Windows), Web Browser",
+  //     publisher: "Blizzard Entertainment",
+  //     developer: "Blizzard Entertainment",
+  //     release_date: "2022-06-02",
+  //     freetogame_profile_url: "https://www.freetogame.com/diablo-immortal",
+  //   },
+  //   {
+  //     id: 388,
+  //     title: "Lord’s Road",
+  //     thumbnail: "https://www.freetogame.com/g/388/thumbnail.jpg",
+  //     short_description:
+  //       "A free-to-play 2D browser-based fantasy MMORPG that features two playable classes.",
+  //     game_url: "https://www.freetogame.com/open/lords-road",
+  //     genre: "MMORPG",
+  //     platform: "Web Browser",
+  //     publisher: "AMZGame",
+  //     developer: "AMZGame",
+  //     release_date: "2015-06-03",
+  //     freetogame_profile_url: "https://www.freetogame.com/lords-road",
+  //   },
+  //   {
+  //     id: 181,
+  //     title: "8BitMMO",
+  //     thumbnail: "https://www.freetogame.com/g/181/thumbnail.jpg",
+  //     short_description:
+  //       "A free to play retro­-style 2D MMO and a giant construction sandbox! ",
+  //     game_url: "https://www.freetogame.com/open/8bitmmo",
+  //     genre: "MMORPG",
+  //     platform: "PC (Windows), Web Browser",
+  //     publisher: "Archive Entertainment ",
+  //     developer: "Archive Entertainment ",
+  //     release_date: "2015-01-26",
+  //     freetogame_profile_url: "https://www.freetogame.com/8bitmmo",
+  //   },
+  // ];
 
   return (
     <div className="free-games">
       <h1>Free games</h1>
 
       <div className="container">
-        {exampleData.map((game) => (
+        {freeGameData.map((game) => (
           <div key={game.id} className="game-content">
             <Link to={game.game_url}>
               <h2 className="game-title">{game.title}</h2>
