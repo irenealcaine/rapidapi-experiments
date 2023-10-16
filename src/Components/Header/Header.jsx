d04bf691829905a54dfffe1764ef1a1c1a7c5472
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-10-16 11:36:42 +0000

    Update Header.jsx and Header.scss

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,13 @@
 import { useContext } from "react";
 import "./Header.scss";
 import logo from "../../Assets/Images/logo.png";
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 import { navbarItems } from "../../Utils/Constants";
 import { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { DarkModeContext } from "../../Context/darkModeContext";
 
 const Header = () => {
-
   const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuActiveClass = isMenuOpen ? "active" : "";
@@ -27,7 +26,12 @@ const Header = () => {
       <ul className={`navigation ${menuActiveClass} ${darkMode ? "dark" : ""}`}>
         {navbarItems.map((navbarItem) => (
           <li key={navbarItem.slug} onClick={() => setIsMenuOpen(false)}>
-            <Link to={`/${navbarItem.slug}`} className={`navigation-item ${location.pathname === `/${navbarItem.slug}` && "active"}`} >
+            <Link
+              to={`/${navbarItem.slug}`}
+              className={`navigation-item ${darkMode ? "dark" : ""} ${
+                location.pathname === `/${navbarItem.slug}` && "active"
+              }`}
+            >
               {navbarItem.name}
             </Link>
           </li>
@@ -37,7 +41,6 @@ const Header = () => {
       <div className={`menu-toggle ${menuActiveClass} `} onClick={toggleMenu}>
         {isMenuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
       </div>
-
     </nav>
   );
 };
