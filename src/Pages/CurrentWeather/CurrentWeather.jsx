bdc293b55b8b4031159f3f2f2036404138fdfce5
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-10-16 17:47:23 +0200

    update 8 files

--- src/Pages/CurrentWeather/CurrentWeather.jsx
+++ src/Pages/CurrentWeather/CurrentWeather.jsx
@@ -1,12 +1,15 @@
 import "./CurrentWeather.scss";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DarkModeContext } from "../../Context/darkModeContext";
+import axios from "axios";
+
 
 // https://www.meteosource.com/documentation
 // https://rapidapi.com/MeteosourceWeather/api/ai-weather-by-meteosource
 
 const Page4 = () => {
   const { darkMode } = useContext(DarkModeContext);
+  const [currentWeather, setCurrentWeather] = useState({})
 
   const exampleData = {
     lat: "39.69951N",
@@ -41,29 +44,61 @@ const Page4 = () => {
     },
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(process.env.REACT_APP_URL_WEATHER, {
+          params: {
+            lat: '39.69951',
+            lon: '-0.70868',
+            timezone: 'auto',
+            language: 'en',
+            units: 'metric'
+          },
+          headers: {
+            "X-RapidAPI-Host": process.env.REACT_APP_WEATHER_HOST,
+            "X-RapidAPI-Key": process.env.REACT_APP_TU_RAPIDAPI_KEY,
+          },
+
+        });
+
+        setCurrentWeather(response.data);
+        console.log(currentWeather.current)
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="current-weather">
       <h1>Current Weather</h1>
       <div className="weather-heading">
         <h2 className={darkMode ? "dark" : ""}>
-          {exampleData.current.summary}
+          {/* {exampleData.current.summary} */}
+          {currentWeather.current.summary}
         </h2>
         <div className="images">
           <img
             src={require(
-              `../../Assets/Images/weather/${exampleData.current.icon_num}.png`,
+              // `../../Assets/Images/weather/${exampleData.current.icon_num}.png`,
+              `../../Assets/Images/weather/${currentWeather.current.icon_num}.png`,
             )}
             alt="weather logo"
           />
           <img
             src={require(
-              `../../Assets/Images/weather/${exampleData.current.icon_num}.png`,
+              `../../Assets/Images/weather/${currentWeather.current.icon_num}.png`,
+              // `../../Assets/Images/weather/${exampleData.current.icon_num}.png`,
             )}
             alt="weather logo"
           />
           <img
             src={require(
-              `../../Assets/Images/weather/${exampleData.current.icon_num}.png`,
+              `../../Assets/Images/weather/${currentWeather.current.icon_num}.png`,
+              // `../../Assets/Images/weather/${exampleData.current.icon_num}.png`,
             )}
             alt="weather logo"
           />
@@ -72,22 +107,29 @@ const Page4 = () => {
       <div className={`weather-content ${darkMode ? "dark" : ""}`}>
         <div className="temperature">
           <h3>Temperature</h3>
-          <p>{exampleData.current.temperature} ºC</p>
+          <p>{currentWeather.current.temperature} ºC</p>
+          {/* <p>{exampleData.current.temperature} ºC</p> */}
         </div>
         <div className="wind">
           <h3>Wind</h3>
           <p>
+            {currentWeather.current.wind.speed} km/h,{" "}
+            {currentWeather.current.wind.dir}
+          </p>
+          {/* <p>
             {exampleData.current.wind.speed} km/h,{" "}
             {exampleData.current.wind.dir}
-          </p>
+          </p> */}
         </div>
         <div className="precipitation">
           <h3>Precipitation</h3>
-          <p>{exampleData.current.precipitation.total} mm</p>
+          <p>{currentWeather.current.precipitation.total} mm</p>
+          {/* <p>{exampleData.current.precipitation.total} mm</p> */}
         </div>
         <div className="humidity">
           <h3>Humidity</h3>
-          <p>{exampleData.current.humidity} %</p>
+          <p>{currentWeather.current.humidity} %</p>
+          {/* <p>{exampleData.current.humidity} %</p> */}
         </div>
       </div>
     </div>
