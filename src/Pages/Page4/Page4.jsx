379730330850966b03c8de5c564d483b0582de6c
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-10-16 08:05:04 +0000

    Update 38 files

--- src/Pages/Page4/Page4.jsx
+++ src/Pages/Page4/Page4.jsx
@@ -1,9 +1,61 @@
 import "./Page4.scss";
+import logo from "../../Assets/Images/weather/11.png";
 
 const Page4 = () => {
+  const exampleData = {
+    lat: "39.69951N",
+    lon: "0.70868W",
+    elevation: 314,
+    timezone: "Europe/Madrid",
+    units: "metric",
+    current: {
+      icon: "overcast",
+      icon_num: 7,
+      summary: "Nublado",
+      temperature: 17.8,
+      feels_like: 16.5,
+      wind_chill: 17.5,
+      dew_point: 16.5,
+      wind: {
+        speed: 0,
+        gusts: 2.2,
+        angle: 291,
+        dir: "ONO",
+      },
+      precipitation: {
+        total: 0,
+        type: "none",
+      },
+      cloud_cover: 99,
+      ozone: 284.69,
+      pressure: 1013,
+      uv_index: 0,
+      humidity: 89,
+      visibility: 14.1,
+    },
+  };
+  console.log(exampleData);
+
   return (
     <div className="page4">
-      <h1>Page 4</h1>
+      <h1>Curent Weather</h1>
+      <h2>{exampleData.current.summary}</h2>
+      <img
+        src={require(
+          `../../Assets/Images/weather/${exampleData.current.icon_num}.png`,
+        )}
+        alt="weather logo"
+      />
+      <h3>Temperature</h3>
+      <p>{exampleData.current.temperature} ºC</p>
+      <h3>Wind</h3>
+      <p>
+        {exampleData.current.wind.speed} km/h, {exampleData.current.wind.dir}
+      </p>
+      <h3>Precipitation</h3>
+      <p>{exampleData.current.precipitation.total} mm</p>
+      <h3>Humidity</h3>
+      <p>{exampleData.current.humidity} %</p>
     </div>
   );
 };
