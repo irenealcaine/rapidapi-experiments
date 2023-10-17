f9079cc52c64365ee0c290065a5526d725cd10ef
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-10-17 06:58:19 +0000

    Create Maket.jsx and MarketExampleData.jsx

---
+++ src/Pages/Market/Maket.jsx
@@ -0,0 +1,9 @@
+const Market = () => {
+  return (
+    <div className="market">
+      <span className="title">Word Hunt</span>
+    </div>
+  );
+};
+
+export default Market;
---
+++ src/Utils/MarketExampleData.jsx
@@ -0,0 +1,208 @@
+export const MarketExampleData = 
+  {
+    "0P0000OQN8": {
+      netChange: {
+        value: 2.8,
+        filtered: false,
+      },
+      previousClosePrice: {
+        value: 251.12,
+        filtered: false,
+      },
+      adjustedClosePrice: {
+        value: 251.12,
+        filtered: false,
+      },
+      name: {
+        value: "Tesla",
+        filtered: false,
+      },
+      percentNetChange: {
+        value: 1.115,
+        filtered: false,
+      },
+      tradingStatus: {
+        value: "Closed",
+        filtered: false,
+      },
+      lastPrice: {
+        value: 253.92,
+        filtered: false,
+        date: {
+          value: "2023-10-16T16:00:11-04:00",
+          filtered: false,
+        },
+      },
+    },
+    "0P0001BT25": {
+      netChange: {
+        value: 1.52,
+        filtered: false,
+      },
+      previousClosePrice: {
+        value: 13.91,
+        filtered: false,
+      },
+      adjustedClosePrice: {
+        value: 13.91,
+        filtered: false,
+      },
+      name: {
+        value: "National Vision Holdings",
+        filtered: false,
+      },
+      percentNetChange: {
+        value: 10.9274,
+        filtered: false,
+      },
+      tradingStatus: {
+        value: "Closed",
+        filtered: false,
+      },
+      lastPrice: {
+        value: 15.43,
+        filtered: false,
+        date: {
+          value: "2023-10-16T16:00:10-04:00",
+          filtered: false,
+        },
+      },
+    },
+    "0P0001F631": {
+      netChange: {
+        value: -6.36,
+        filtered: false,
+      },
+      previousClosePrice: {
+        value: 98.3,
+        filtered: false,
+      },
+      adjustedClosePrice: {
+        value: 98.3,
+        filtered: false,
+      },
+      name: {
+        value: "Moderna",
+        filtered: false,
+      },
+      percentNetChange: {
+        value: -6.47,
+        filtered: false,
+      },
+      tradingStatus: {
+        value: "Closed",
+        filtered: false,
+      },
+      lastPrice: {
+        value: 91.94,
+        filtered: false,
+        date: {
+          value: "2023-10-16T16:00:11-04:00",
+          filtered: false,
+        },
+      },
+    },
+    "0P000000B7": {
+      netChange: {
+        value: 2.76,
+        filtered: false,
+      },
+      previousClosePrice: {
+        value: 129.79,
+        filtered: false,
+      },
+      adjustedClosePrice: {
+        value: 129.79,
+        filtered: false,
+      },
+      name: {
+        value: "Amazon.com",
+        filtered: false,
+      },
+      percentNetChange: {
+        value: 2.1265,
+        filtered: false,
+      },
+      tradingStatus: {
+        value: "Closed",
+        filtered: false,
+      },
+      lastPrice: {
+        value: 132.55,
+        filtered: false,
+        date: {
+          value: "2023-10-16T16:00:11-04:00",
+          filtered: false,
+        },
+      },
+    },
+    "0P000000GY": {
+      netChange: {
+        value: -0.13,
+        filtered: false,
+      },
+      previousClosePrice: {
+        value: 178.85,
+        filtered: false,
+      },
+      adjustedClosePrice: {
+        value: 178.85,
+        filtered: false,
+      },
+      name: {
+        value: "Apple",
+        filtered: false,
+      },
+      percentNetChange: {
+        value: -0.0727,
+        filtered: false,
+      },
+      tradingStatus: {
+        value: "Closed",
+        filtered: false,
+      },
+      lastPrice: {
+        value: 178.72,
+        filtered: false,
+        date: {
+          value: "2023-10-16T16:00:12-04:00",
+          filtered: false,
+        },
+      },
+    },
+    "0P000004C0": {
+      netChange: {
+        value: 1.16,
+        filtered: false,
+      },
+      previousClosePrice: {
+        value: 32.11,
+        filtered: false,
+      },
+      adjustedClosePrice: {
+        value: 32.11,
+        filtered: false,
+      },
+      name: {
+        value: "Pfizer",
+        filtered: false,
+      },
+      percentNetChange: {
+        value: 3.6126,
+        filtered: false,
+      },
+      tradingStatus: {
+        value: "Closed",
+        filtered: false,
+      },
+      lastPrice: {
+        value: 33.27,
+        filtered: false,
+        date: {
+          value: "2023-10-16T19:00:01-04:00",
+          filtered: false,
+        },
+      },
+    },
+  },
+;
