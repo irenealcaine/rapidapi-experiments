c3c84cbc4ab33446b2975143aaf5187c7f1f6aa8
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-10-17 17:42:01 +0200

    update Market.jsx

--- src/Pages/Market/Market.jsx
+++ src/Pages/Market/Market.jsx
@@ -1,25 +1,51 @@
+import { useEffect, useState } from "react";
 import { MarketExampleData } from "../../Utils/MarketExampleData";
 import "./Market.scss";
+import axios from "axios";
 // https://rapidapi.com/apidojo/api/morning-star
 
 const Market = () => {
+
+  const [marketData, setMarketData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(process.env.REACT_APP_URL_MARKET, {
+          params: {
+            performanceIds: '0P0000OQN8,0P000000GY'
+          },
+          headers: {
+            "X-RapidAPI-Host": process.env.REACT_APP_MARKET_HOST,
+            "X-RapidAPI-Key": process.env.REACT_APP_TU_RAPIDAPI_KEY,
+          },
+        });
+
+        setJokes(response.data);
+        console.log(jokes);
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="market">
       <h1>Market</h1>
       {console.log(MarketExampleData)}
       {Object.values(MarketExampleData).map((market) => (
         <div
-          className={`market-item ${
-            market.percentNetChange.value < 0 && "negative"
-          }`}
+          className={`market-item ${market.percentNetChange.value < 0 && "negative"
+            }`}
         >
           <h2 className="name">{market.name.value}</h2>
           <p className="value">
             {market.lastPrice.value} ${" "}
             <span
-              className={`percentage ${
-                market.percentNetChange.value < 0 && "negative"
-              }`}
+              className={`percentage ${market.percentNetChange.value < 0 && "negative"
+                }`}
             >
               {"("}
               {market.percentNetChange.value > 0 ? "+" : ""}
